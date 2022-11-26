30ba6283aba28cee49a85a0bf315b2c042b60d5b
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-11-27 00:01:50 +0100

    Improve hovering

--- Assets/Scripts/OnHover.cs
+++ Assets/Scripts/OnHover.cs
@@ -6,15 +6,22 @@ using UnityEngine.EventSystems;
 public class OnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public UnityEngine.UI.Image image;
+
+    private Color initialColor;
     // Start is called before the first frame update
     void Start()
     {
-        image = GetComponent<UnityEngine.UI.Image>();
+        if (image == null)
+        {
+            image = GetComponent<UnityEngine.UI.Image>();
+        }
+        initialColor = image.color;
+        image.color = Color.clear;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        image.color = Color.white;
+        image.color = initialColor;
     }
 
     public void OnPointerExit(PointerEventData eventData)
