605f2eead1567804211b1a0926a8056747613ffe
boniffacy <94139663+boniffacy@users.noreply.github.com>
2022-11-27 07:25:34 +0100

    Escape from list through bg
    List closes when you click on the background.

--- Assets/Scripts/OnEscape.cs
+++ Assets/Scripts/OnEscape.cs
@@ -15,5 +15,14 @@ public class OnEscape : MonoBehaviour
         {
             onEscape.Invoke();
         }
+        // Escape when clicked dark background
+        if  (GameObject.Find("LetterHolder").GetComponent<DeleteLetterOrQuit>().letter != null
+            && Camera.main.GetComponent<Camera>().orthographicSize > 3.0f
+            && (Input.mousePosition.x < 600f || Input.mousePosition.x > 1320f
+            ||  Input.mousePosition.y < 15f || Input.mousePosition.y > 1065f)
+            && Input.GetMouseButtonDown(0))
+        {
+            onEscape.Invoke();
+        }
     }
 }
