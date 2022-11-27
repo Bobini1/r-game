c18165cfc53f3914bf69408c8b9b14523749b7da
Condotierro <118808096+Condotierro@users.noreply.github.com>
2022-11-27 02:54:13 +0100

    Merge branch 'main' of https://github.com/Bobini1/r-game

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,9 +10,26 @@ public class ScoreManager : MonoBehaviour
     int comboScore;
     int maxScore;
     int failCounter = 0;
+
+
+    public GameObject progressBar;
+    float progressBarValue;
+    float progressBarMaxValue;
+
+    SongManager songManager;
+    private void Awake()
+    {
+        songManager = FindObjectOfType<SongManager>();
+    }
     void Start()
     {
         comboScore = 0;
+        progressBarValue = 0;
+        if(songManager != null)
+        {
+            progressBarMaxValue = songManager.GetSongNotes();
+        }
+        Debug.Log(progressBarMaxValue);
     }
     public void Hit()
     {
@@ -25,9 +42,9 @@ public class ScoreManager : MonoBehaviour
         failCounter++;
         //replace 5 with variable containing track specific max consecutive fails
         //this has to be done here in order to preserve the actual combo, max scores
-        if (failCounter >= 5)
+        if (failCounter >= 200)
         {
-            SongManager songManager = FindObjectOfType<SongManager>();
+            
             Debug.Log("done");
             if(songManager != null)
             {
@@ -45,6 +62,14 @@ public class ScoreManager : MonoBehaviour
     private void Update()
     {
         scoreText.text = comboScore.ToString();
+        float speed = 1f;
+        speed = progressBarMaxValue /360; //Check whether it works for other tracks!!!!
+        
+        //max X -18.5 - Final X 0
+        if(progressBar.transform.position.x <= 0)
+        {
+            progressBar.transform.position += new Vector3(1, 0) * Time.deltaTime * speed;
+        }
     }
 
     public int getComboScore()
